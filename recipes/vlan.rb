2258d1c1a773f44c9cb3877558384814f0e85e93
Jere Julian <jere@arista.com>
2016-03-21 09:07:33 -0400

    Change description and add another test vlan

--- recipes/vlan.rb
+++ recipes/vlan.rb
@@ -7,8 +7,14 @@
 
 eos_vlan '5' do
   #action :create
-  vlan_name 'jere05'
+  vlan_name 'Test_VLAN_5'
   enable true
-  # trunk_groups ['mlag_ctl', 'test']
   trunk_groups %w(mlag_ctl test)
+end
+
+eos_vlan '6' do
+  action :create
+  vlan_name 'Test_VLAN_6'
+  enable true
+  trunk_groups ['mlag_ctl', 'test']
 end
