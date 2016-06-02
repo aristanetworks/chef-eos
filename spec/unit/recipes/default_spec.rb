0378b93cf2fa615bf09b7ce71009ed34749dd264
Jere Julian <jere@arista.com>
2016-06-02 14:41:22 -0700

    Add initial spec tests for eos::default

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: eos
 # Spec:: default
 #
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+# Copyright (c) 2016 Arista Networks, All Rights Reserved.
 
 require 'spec_helper'
 
@@ -13,8 +13,36 @@ describe 'eos::default' do
       runner.converge(described_recipe)
     end
 
-    it 'converges successfully' do
+    it 'converges successfully with eAPI configured' do
+      stub_command("/usr/bin/FastCli -p 15 -c \"show running-config\" | grep unix-socket").and_return(true)
       expect { chef_run }.to_not raise_error
+    end
+
+    it 'converges successfully with eAPI not configured' do
+      stub_command("/usr/bin/FastCli -p 15 -c \"show running-config\" | grep unix-socket").and_return(false)
+      expect { chef_run }.to_not raise_error
+    end
+
+    it 'configures eAPI for unix-sockets when not detected' do
+      stub_command("/usr/bin/FastCli -p 15 -c \"show running-config\" | grep unix-socket").and_return(false)
+      expect(chef_run).to run_execute('Enable eAPI')
+    end
+
+    it 'does not reconfigure eAPI for unix-sockets when detected' do
+      stub_command("/usr/bin/FastCli -p 15 -c \"show running-config\" | grep unix-socket").and_return(true)
+      expect(chef_run).to_not run_execute('Enable eAPI')
+    end
+
+    it 'ensures base chef files are persistent' do
+      stub_command("/usr/bin/FastCli -p 15 -c \"show running-config\" | grep unix-socket").and_return(true)
+      expect(chef_run).to create_directory('/persist/sys/chef').with(
+        owner: 'root',
+        group: 'root',
+        mode: '0755'
+      )
+      expect(chef_run).to create_link('/etc/chef').with(
+        to: '/persist/sys/chef'
+      )
     end
   end
 end
