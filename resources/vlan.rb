a1ffad6c27d8a8010709279dc190ded67965c5f8
Jere Julian <jere@arista.com>
2016-06-02 14:14:25 -0700

    clean up eos_vlan resource

--- resources/vlan.rb
+++ resources/vlan.rb
@@ -8,25 +8,15 @@
 #   end
 
 property :vlan, Fixnum, name_property: true
-# property :vlan, Fixnum, required: true
 property :vlan_name, String
-#property :switch_name, String, default: 'localhost'
 property :switch_name, String, desired_state: false
 property :enable, kind_of: [TrueClass, FalseClass], default: true
 property :trunk_groups, Array
-# property :trunk_groups, Array, default: []
-# property :trunk_groups, Array, default: lazy { [] }
-# property :trunk_groups, Array, default: lazy { default_trunk_groups }
 
 # Defaults to the first action
 default_action :create
 
-#require_relative '_eos_eapi'
 begin
-  # Include gems vendored into this cookbook in the LOAD_PATH
-  $LOAD_PATH.unshift(*Dir[::File.expand_path(
-    '../../files/default/vendor/gems/**/lib', __FILE__)]
-  )
   require 'rbeapi'
 rescue LoadError
   msg = 'Unable to load rbeapi rubygem'
@@ -47,8 +37,6 @@ def switch
   @switch = Rbeapi::Client.connect_to(connection_name)
 end
 
-require 'pry'
-
 load_current_value do
   vlans = switch.api('vlans').getall
   current_value_does_not_exist! unless vlans.key?(vlan)
@@ -57,30 +45,30 @@ load_current_value do
   state = vlans[vlan][:state]
   enable state == 'active' ? true : false
   trunk_groups vlans[vlan][:trunk_groups]
-
-  # binding.pry
 end
 
 action :create do
-  # converge_by "Creating vlan #{vlan}" do
   converge_if_changed :vlan do
     switch.api('vlans').create(vlan)
   end
+
   converge_if_changed :vlan_name do
     switch.api('vlans').set_name(vlan, value: vlan_name)
   end
+
   converge_if_changed :enable do
     state = enable == true ? 'active' : 'suspend'
     switch.api('vlans').set_state(vlan, value: state)
   end
+
   converge_if_changed :trunk_groups do
-    # binding.pry
     # trunk_groups needs more complex logic to reconcile with add/remove
     add = new_resource.trunk_groups - current_resource.trunk_groups
     remove = current_resource.trunk_groups - new_resource.trunk_groups
     add.each do |group|
       switch.api('vlans').add_trunk_group(vlan, group)
     end
+
     remove.each do |group|
       switch.api('vlans').remove_trunk_group(vlan, group)
     end
@@ -88,7 +76,6 @@ action :create do
 end
 
 action :delete do
-  #return unless current_resource
   converge_by "Deleting vlan #{vlan}" do
     switch.api('vlans').destroy(vlan)
   end
