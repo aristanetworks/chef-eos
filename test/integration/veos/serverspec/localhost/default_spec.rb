13d774b41430ab41c3c23a52b2583dee44f7ecf5
Jere Julian <jere@arista.com>
2016-06-29 15:21:21 -0400

    Add initial real spec tests in Kitchen

--- test/integration/veos/serverspec/localhost/default_spec.rb
+++ test/integration/veos/serverspec/localhost/default_spec.rb
@@ -3,9 +3,6 @@ require 'spec_helper'
 # http://serverspec.org/resource_types.html
 
 describe 'eos::default' do
-  #it 'creates a link from /etc/chef to /persist/sys/chef' do
-  #skip 'Replace this with meaningful tests'
-
   describe file('/persist/sys/chef') do
     it { should be_directory }
     it { should be_mode 755 }
@@ -16,8 +13,9 @@ describe 'eos::default' do
     it { should be_linked_to '/persist/sys/chef' }
   end
 
-  describe command('/usr/bin/FastCli -p 15 -c "show running-config section management api http-commands"') do
-    its(:stdout) { should contain('protocol unix-socket')}
-    its(:stdout) { should contain('no shutdown')}
+  cmd = 'show running-config section management api http-commands'
+  describe command("/usr/bin/FastCli -p 15 -c \"#{cmd}\"") do
+    its(:stdout) { should contain('protocol unix-socket') }
+    its(:stdout) { should contain('no shutdown') }
   end
 end
